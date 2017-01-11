34c300c098cd4ae41273341e880f26aa8cb101f9
Donnie Ellstrom <dsvene@gmail.com>
2017-01-11 07:56:35 -0800

    Add async module, get info from OMDB api, render info to result page

--- controllers/media.js
+++ controllers/media.js
@@ -1,19 +1,69 @@
 var express = require('express');
 var router = express.Router();
+var async = require('async');
 var db = require('../models');
 var request = require('request');
 
-router.get('/', function(req, res) {
-  var netflixUrl = 'http://netflixroulette.net/api/api.php?actor=Zach%20Braff';
+var userResults = [];
+// var ids = [];
+var totalInfo = [];
+var funcs = [];
+//Get request details
 
-  request(netflixUrl, function(error, response, body) {
-    console.log(error, response, body);
-    var userResult = body;
-    //The body is displayed clean in chrome via JSON plugin, not natively
-    res.send(body);
-    console.log(userResult);
-    // console.log(userResult.show_title);
-    // res.render('index', { userResult: userResult });
+function getDetails(id, callback) {
+  console.log("THIS IS ID INSIDE OF GET getDetails: ", id);
+  var url = 'http://www.omdbapi.com/?i=' + id + '&plot=short&r=json';
+  request(url, function(error, response, body) {
+      callback(null, JSON.parse(body));
+  });
+}
+
+// //POST query from user search input
+// router.post('/', function(req, res) {
+//   var omdbUrl = 'http://www.omdbapi.com/?s=' + req.body.search + '&y=&plot=short&r=json';
+//
+//   request(omdbUrl, function(error, response, body) {
+//     userResults = JSON.parse(body).Search;
+//     userResults.forEach(function(result) {
+//       ids.push(result['imdbID']);
+//       return ids;
+//     });
+//     console.log("THIS IS ids ARRAY", ids);
+//     for(i = 0; i < ids.length; i++) {
+//       getDetails(ids[i]);
+//     }
+//     res.render('result', { userResults: userResults });
+//   });
+// });
+
+
+//POST query from user search input
+router.post('/', function(req, res) {
+  var omdbUrl = 'http://www.omdbapi.com/?s=' + req.body.search + '&y=&plot=short&r=json';
+
+  request(omdbUrl, function(error, response, body) {
+    userResults = JSON.parse(body).Search;
+
+    var ids = userResults.map(function(result) {
+      return result['imdbID'];
+    });
+
+    console.log("THIS IS ids ARRAY", ids);
+
+    for(var i = 0; i < ids.length; i++) {
+      var id = ids[i];
+      console.log("THIS IS ID before getDetails ", id);
+      var func = function(callback) {
+        getDetails(id, callback);
+      console.log("THIS IS ID after getDetails ", id);
+      };
+      funcs.push(func);
+    }
+
+    async.parallel(funcs, function(err, results) {
+      // console.log("FINAL RESULTS! ", results);
+      res.render('result', { userResults: results });
+    });
   });
 });
 
